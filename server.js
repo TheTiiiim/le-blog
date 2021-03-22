2da18a043e1b084d6c11c40c48d0aa8b5410fe3e
Tim Scott <0timothy.scott0@gmail.com>
2021-03-21 21:28:25 -0400

    hello world page

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-
+const path = require("path");
+const express = require("express");
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
 sequelize.sync({ force: false }).then(() => {
-    console.log("connected to database");
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
