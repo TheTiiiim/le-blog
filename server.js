03bbf3d0b963711e026ae1992c173cfe8c182d51
Tim Scott <0timothy.scott0@gmail.com>
2021-03-27 17:26:20 -0400

    update homepage to display users

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
 const cookieParser = require("cookie-parser");
+
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 
