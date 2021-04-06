da152ee967a85d45782b45b7c79c2659fb498aeb
Tim Scott <0timothy.scott0@gmail.com>
2021-04-06 00:03:36 -0400

    each user has dedicated page

--- controllers/user.js
+++ controllers/user.js
@@ -7,9 +7,34 @@ const { User } = require("../models");
 
 router.get("/dashboard", requireCookie, async (req, res) => {
     try {
-        res.render("dashboard", { title: "Dashboard" });
+        res.redirect(`/user/${req.cookieUserData.id}`);
     } catch {
         res.redirect("/login");
+    }
+});
+
+router.get("/user/:id", async (req, res) => {
+    try {
+        let userData;
+        // if viewing user in cookie
+        if (req.cookieUserData.id === parseInt(req.params.id)) {
+            // set userData to match
+            userData = req.cookieUserData;
+            // set dashboard flag
+            res.locals.dashboard = true;
+        } else {
+            // get user
+            userData = await User.findByPk(req.params.id);
+            // ensure user exists
+            if (!userData) {
+                throw Error("no user");
+            }
+        }
+        // get object usable by template engine
+        const user = userData.get({ plain: true });
+        res.render("user", { title: `${user.name}'s page`, user });
+    } catch {
+        res.render("user", { title: "User not found" });
     }
 });
 
