7a456da6f39a55a9369699dd2b4a7d2d3f83ebca
Tim Scott <0timothy.scott0@gmail.com>
2021-04-10 10:56:15 -0400

    display posts with user link

--- controllers/post.js
+++ controllers/post.js
@@ -1,9 +1,24 @@
 const router = require("express").Router();
 
 // const { requireCookie } = require("../middlewares/auth");
-const { Post } = require("../models");
+const { Post, User } = require("../models");
 
 // "/" endpoint
+
+router.get("/post/:id", async (req, res) => {
+    try {
+        // get post
+        const postData = await Post.findByPk(req.params.id, { include: User });
+        // check post exists
+        if (!postData) {
+            throw Error("No post");
+        }
+        const post = postData.get({ plain: true });
+        res.render("post", { title: post.title, post });
+    } catch {
+        res.render("post", { title: "Post not found" });
+    }
+});
 
 router.get("/posts", async (req, res) => {
     const dbUserData = await Post.findAll();
